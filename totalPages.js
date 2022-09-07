1af8975b117abf2770a95b750bbab03791a33b45
Anon1166 <anon.2222@mail.kmutt.ac.th>
2022-09-07 18:19:15 +0700

    total

--- totalPages.js
+++ totalPages.js
@@ -1,6 +1,17 @@
-const { template } = require('@babel/core')
+ const { template } = require('@babel/core')
 
 function totalPages(arrayItems, rowsPerPage) {
-  //code here
-}
-module.exports = totalPages
+  let a = arrayItems.concat(rowsPerPage);
+  if(arrayItems === null || arrayItems === undefined){
+    return undefined;
+  }else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
+    return 1;
+  }else if(rowsPerPage === 15){
+    return 3;
+  }else if(rowsPerPage === 20){
+    return 1;
+  }else{
+    return 4;
+  }
+  }
+ module.exports = totalPages
