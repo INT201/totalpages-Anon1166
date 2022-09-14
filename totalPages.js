811b23be23ac69a5c035af52d5552c611304bd7c
Anon1166 <anon.2222@mail.kmutt.ac.th>
2022-09-14 20:42:16 +0700

    V6

--- totalPages.js
+++ totalPages.js
@@ -5,12 +5,15 @@ function totalPages(arrayItems, rowsPerPage) {
     return undefined;
   }else if(rowsPerPage === null || rowsPerPage === undefined || rowsPerPage === 0){
     return 1;
-  }else if(rowsPerPage === 15){
-    return 3;
-  }else if(rowsPerPage === 20){
-    return 1;
   }else{
-    return 4;
+    return Math.ceil(arrayItems.length/rowsPerPage)
   }
-  }
+
+}
+  
  module.exports = totalPages
+console.log(totalPages([
+  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35
+],
+15))
